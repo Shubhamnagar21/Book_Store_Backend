8c6e0b34728b2b317be1bc1fa808ff61750005e8
Shubhamnagar21 <shubhamnagar5000@gmail.com>
2025-01-28 01:02:09 +0530

    final commit'

---
+++ index.js
@@ -0,0 +1,44 @@
+import express, { request, response, urlencoded } from "express";
+import { PORT, mongodbURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoutes.js";
+import { config } from "dotenv";
+config({ path: "./.env" });
+import cors from "cors";
+
+
+const app = express();
+
+// middleWare for parsing request body
+app.use(express.json());
+
+// Middle for handling CORS Policy
+// Option 1: Allow all origins with Default of Cors(*)
+// app.use(cors());
+// Option 2: Allow custom Origins
+app.use(cors({
+  origin:['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+
+// Home Route
+app.get("/", (request, response) => {
+  // console.log(request);
+  return response.status(234).send("Hello World");
+});
+
+// Routes
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(process.env.mongodbURL)
+  .then(() => {
+    console.log("database connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listing to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
